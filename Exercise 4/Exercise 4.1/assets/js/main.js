1401b684f56d2ab7be97994c151dce6f0813521b
julian07chok <105684522@student.swin.edu.au>
2026-09-10 17:44:02 +0800

    Add a test svg rectangle

--- Exercise 4/Exercise 4.1/assets/js/main.js
+++ Exercise 4/Exercise 4.1/assets/js/main.js
@@ -3,3 +3,12 @@ const svg = d3.select(".responsive-svg-container")
     .append("svg")
       .attr("viewBox", "0 0 1200 1600")
       .style("border", "1px solid black");
+
+// Step 4: Add a test svg rectangle
+svg
+  .append("rect")
+    .attr("x", 10)
+    .attr("y", 10)
+    .attr("width", 414)
+    .attr("height", 16)
+    .attr("fill", "blue");
