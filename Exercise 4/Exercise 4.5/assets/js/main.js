305a76233cdac35f94ee655574ff0aac9ac3625f
julian07chok <105684522@student.swin.edu.au>
2026-09-11 14:07:32 +0800

    Exercise 4.5

--- Exercise 4/Exercise 4.5/assets/js/main.js
+++ Exercise 4/Exercise 4.5/assets/js/main.js
@@ -1,7 +1,7 @@
 // Exercise 4.3 Step 2: Create svg object within the new div 
 const svg = d3.select(".responsive-svg-container")
     .append("svg")
-      .attr("viewBox", "0 0 440 40")
+      .attr("viewBox", "0 0 1200 1600")
       .style("border", "1px solid black");
 
 // Exercise 4.4 Step 1: Use row conversion function, d3.csv(), to give D3 access to data
@@ -45,3 +45,38 @@ d3.csv("data/tvBrandCount.csv", d => {
   // Call drawBarChart with the loaded data
   drawBarChart(data);
 });
+
+// Exercise 4.5 Step 1:  Bind the data to DOM elements
+/*
+const drawBarChart = data => {
+  svg
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("class", d => {
+      console.log(d);
+      return `bar bar-${d.count}`;
+    })
+ };
+ */
+
+// Exercise 4.5 Step 2: Make your data visible: Add attributes (.attr) to your bar chart for width, height and fill
+const drawBarChart = data => {
+  const barHeight = 20;
+
+  const barSpacing = 5;
+
+  svg
+  .selectAll("rect")
+  .data(data)
+  .join("rect")
+  .attr("class", d => {
+          console.log(d);
+          return `bar bar-${d.count}`;
+        })
+  .attr("width", d => d.count)
+  .attr("height", barHeight)
+  .attr("fill", "blue")
+  .attr("x", 0)
+  .attr("y", (d, i) => i * (barHeight + barSpacing));
+};
