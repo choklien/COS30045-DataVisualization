63de9e9c1cc430036cd2b35c89652f85cfac734f
julian07chok <105684522@student.swin.edu.au>
2026-09-11 11:50:38 +0800

    make the view box smaller

--- Exercise 4/Exercise 4.3/assets/js/main.js
+++ Exercise 4/Exercise 4.3/assets/js/main.js
@@ -1,7 +1,7 @@
 // Step 2: Create svg object within the new div 
 const svg = d3.select(".responsive-svg-container")
     .append("svg")
-      .attr("viewBox", "0 0 1200 1600")
+      .attr("viewBox", "0 0 440 40")
       .style("border", "1px solid black");
 
 // Step 3: Add a test svg rectangle
