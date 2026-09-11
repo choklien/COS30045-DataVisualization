36ba58a97eb03b119beec799317b6e11184ee9ed
julian07chok <105684522@student.swin.edu.au>
2026-09-11 12:12:03 +0800

    Exercise 4.4 step 2 & 3

--- Exercise 4/Exercise 4.4/assets/js/main.js
+++ Exercise 4/Exercise 4.4/assets/js/main.js
@@ -14,7 +14,37 @@ svg
     .attr("fill", "blue");
 
 // Exercise 4.4 Step 1: Use row conversion function, d3.csv(), to give D3 access to data
+/*
 d3.csv("data/tvBrandCount.csv", d => {
   console.log(d); 
 }
 );
+*/
+
+// Exercise 4.4 Step 2: Check browser console tab for java objects created from your data set
+/*
+d3.csv("data/tvBrandCount.csv", d => {
+  return {
+    brand: d.brand,
+    count: +d.count
+  };
+})
+.then(data => {
+  console.log(data);
+});
+*/
+
+// Exercise 4.4 Step 3: Finding information about the data set
+d3.csv("data/tvBrandCount.csv", d => {
+  return {
+    brand: d.brand,
+    count: +d.count
+  };
+})
+.then(data => {
+  console.log(data);
+  console.log(data.length);
+  console.log(d3.max(data, d => d.count));
+  console.log(d3.min(data, d => d.count));
+  console.log(d3.extent(data, d => d.count)); //=> array with min and max
+});
