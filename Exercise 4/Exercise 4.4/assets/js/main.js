7a75f94d7bd27378c006aeaf454c52add40bb1db
julian07chok <105684522@student.swin.edu.au>
2026-09-11 14:49:39 +0800

    Remove 4.2

--- Exercise 4/Exercise 4.4/assets/js/main.js
+++ Exercise 4/Exercise 4.4/assets/js/main.js
@@ -1,7 +1,7 @@
 // Exercise 4.3 Step 2: Create svg object within the new div 
 const svg = d3.select(".responsive-svg-container")
     .append("svg")
-      .attr("viewBox", "0 0 440 40")
+      .attr("viewBox", "0 0 1200 1600")
       .style("border", "1px solid black");
 
 // Exercise 4.4 Step 1: Use row conversion function, d3.csv(), to give D3 access to data
@@ -37,7 +37,6 @@ d3.csv("data/tvBrandCount.csv", d => {
   console.log(data.length);
   console.log(d3.max(data, d => d.count));
   console.log(d3.min(data, d => d.count));
-  console.log(d3.extent(data, d => d.count)); //=> array with min and max
 
   // Sort descending by count (largest first)
   data.sort((a, b) => b.count - a.count);
