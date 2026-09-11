21746984b4994949eb430bc0585a95028c1cda07
julian07chok <105684522@student.swin.edu.au>
2026-09-11 11:28:11 +0800

    Exercise 4.3

--- Exercise 4/Exercise 4.3/assets/js/main.js
+++ Exercise 4/Exercise 4.3/assets/js/main.js
@@ -1,24 +1,14 @@
-// Step 2: Apply a style to html element using D3
-d3.select("h1")
-  .style("color", "lightblue");
+// Step 2: Create svg object within the new div 
+const svg = d3.select(".responsive-svg-container")
+    .append("svg")
+      .attr("viewBox", "0 0 1200 1600")
+      .style("border", "1px solid black");
 
-d3.select("h2")
-  .style("color", "pink");
-
-// Step 3: Append an element using D3
-d3.select("div")
-  .append("p")
-    .text("Purchasing a low energy consumption TV will help with your energy bills!");
-
-
-// Step 4: Append an SVG element using D3
-d3.select("svg")
-  .append("rect");
-
-d3.select("svg")
+// Step 3: Add a test svg rectangle
+svg
   .append("rect")
-   .attr("x", 50)
-   .attr("y", 50)
-   .attr("width", 100)
-   .attr("height", 30)
-   .style("fill", "lightgreen");
+    .attr("x", 10)
+    .attr("y", 10)
+    .attr("width", 414)
+    .attr("height", 16)
+    .attr("fill", "blue");
