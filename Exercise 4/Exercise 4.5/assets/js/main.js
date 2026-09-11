0626156a47ffa8ad9432a237600aaa2201d27a4e
julian07chok <105684522@student.swin.edu.au>
2026-09-11 14:23:51 +0800

    Exercise 6

--- Exercise 4/Exercise 4.5/assets/js/main.js
+++ Exercise 4/Exercise 4.5/assets/js/main.js
@@ -1,8 +1,10 @@
 // Exercise 4.3 Step 2: Create svg object within the new div 
+/*
 const svg = d3.select(".responsive-svg-container")
     .append("svg")
       .attr("viewBox", "0 0 1200 1600")
       .style("border", "1px solid black");
+*/
 
 // Exercise 4.4 Step 1: Use row conversion function, d3.csv(), to give D3 access to data
 /*
@@ -24,6 +26,11 @@ d3.csv("data/tvBrandCount.csv", d => {
   console.log(data);
 });
 */
+/* Exercise 4.6 - Change viewBox size */
+const svg = d3.select(".responsive-svg-container") 
+    .append("svg")
+      .attr("viewBox", "0 0 500 500")
+      .style("border", "1px solid black");
 
 // Exercise 4.4 Step 3: Finding information about the data set
 d3.csv("data/tvBrandCount.csv", d => {
@@ -66,6 +73,24 @@ const drawBarChart = data => {
 
   const barSpacing = 5;
 
+  // Exercise 4.6 Step 1: Add Linear scale for count data
+  const xScale = d3.scaleLinear()
+  .domain([0, 1100])
+  .range([0, 500]);
+
+  // Exercise 4.6 Step 2: Use a band scale to calculate bar thickness based on number of categories
+  /*
+  const yScale = d3.scaleBand()
+ .domain(data.map(d => d.brand))
+ .range([0, 1600]);
+ */
+
+ // add padding to Y scale to create space between bars
+  const yScale = d3.scaleBand()
+    .domain(data.map(d => d.brand))
+    .range([0, 500])
+    .padding(0.1);
+
   svg
   .selectAll("rect")
   .data(data)
@@ -74,9 +99,21 @@ const drawBarChart = data => {
           console.log(d);
           return `bar bar-${d.count}`;
         })
-  .attr("width", d => d.count)
-  .attr("height", barHeight)
+  
+  //.attr("width", d => d.count)
+  // Exercise 4.6 Step 2: Use Linear scale to calculate bar widths
+  .attr("width", d => xScale(d.count))
+
+  //.attr("height", barHeight)
+  .attr("height", yScale.bandwidth()) // Adjust the thickness of the bar
+
   .attr("fill", "blue")
   .attr("x", 0)
-  .attr("y", (d, i) => i * (barHeight + barSpacing));
+
+  //.attr("y", (d, i) => i * (barHeight + barSpacing));
+  .attr("y", (d, i) => yScale(d.brand)); // adjust the location of the bar relative to the y-axis location
+
+
+  
+
 };
